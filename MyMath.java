77d8b2209580d73bb28414d6896bb36e4278b667
Hacker459954 <lionyst+1@proton.me>
2024-07-27 12:54:17 +0530

    Create MyMath.java

---
+++ MyMath.java
@@ -0,0 +1,12 @@
+package myscience.tools;
+public class MyMath {
+  String name;
+  int age;
+  public MyMath(String name, int age){
+    this.name = name;
+    this.age = age;
+  }
+  public static void main(String[] args){
+  }
+
+}
